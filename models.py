08885197ef9c5eb5600cace612992696d7308e33
Seyda <seydamollaoglari@gmail.com>
2026-05-28 22:13:23 +0300

    Revert "KAN-xx: İlişkisel veri modelleri - Bolge ve AfetOlayi tablolari eklendi"
    This reverts commit 2b37bb39717401559c602637ce8c7742baa6e1d2.

--- models.py
+++ models.py
@@ -3,59 +3,23 @@ from datetime import datetime, timezone
 
 db = SQLAlchemy()
 
-
-class Bolge(db.Model):
-    __tablename__ = 'bolgeler'
-
-    id = db.Column(db.Integer, primary_key=True)
-    ad = db.Column(db.String(100), nullable=False)
-    il = db.Column(db.String(50), nullable=False)
-    ilce = db.Column(db.String(50), nullable=True)
-    nufus = db.Column(db.Integer, nullable=False)
-    koordinat_lat = db.Column(db.Float, nullable=True)
-    koordinat_lon = db.Column(db.Float, nullable=True)
-
-    afet_olaylari = db.relationship('AfetOlayi', back_populates='bolge', lazy=True)
-
-    def __repr__(self):
-        return f'<Bolge {self.ad} - {self.il}>'
-
-
-class AfetOlayi(db.Model):
-    __tablename__ = 'afet_olaylari'
-
-    id = db.Column(db.Integer, primary_key=True)
-    bolge_id = db.Column(db.Integer, db.ForeignKey('bolgeler.id'), nullable=False)
-    deprem_buyuklugu = db.Column(db.Float, nullable=False)
-    bina_yikim_orani = db.Column(db.Float, nullable=False)   # 0.0 - 1.0
-    hava_sicakligi = db.Column(db.Float, nullable=False)
-    ulasim_durumu = db.Column(db.Integer, nullable=False)    # 0=kapali, 1=kismi, 2=acik
-    yasli_nufus_orani = db.Column(db.Float, nullable=False)  # 0.0 - 1.0
-    olay_tarihi = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
-
-    bolge = db.relationship('Bolge', back_populates='afet_olaylari')
-    tahmin_kayitlari = db.relationship('TahminKaydi', back_populates='afet_olayi', lazy=True)
-
-    def __repr__(self):
-        return f'<AfetOlayi {self.id} - Buyukluk:{self.deprem_buyuklugu}>'
-
-
 class TahminKaydi(db.Model):
     __tablename__ = 'tahmin_kayitlari'
 
     id = db.Column(db.Integer, primary_key=True)
-    afet_olayi_id = db.Column(db.Integer, db.ForeignKey('afet_olaylari.id'), nullable=False)
-
-    # Tahmin sonuçları (ihtiyaç miktarları)
-    acil_barinma = db.Column(db.Integer, nullable=False)   # kişi sayısı
-    gida = db.Column(db.Integer, nullable=False)            # öğün sayısı
-    su = db.Column(db.Integer, nullable=False)              # litre
-    medikal = db.Column(db.Integer, nullable=False)         # kişi sayısı
-    ekip = db.Column(db.Integer, nullable=False)            # ekip sayısı
-
+    nufus = db.Column(db.Integer, nullable=False)
+    bina_yikim_orani = db.Column(db.Float, nullable=False)
+    hava_sicakligi = db.Column(db.Float, nullable=False)
+    ulasim_durumu = db.Column(db.Integer, nullable=False)
+    yasli_nufus_orani = db.Column(db.Float, nullable=False)
+    
+    barinma_sonucu = db.Column(db.Integer, nullable=False)
+    gida_sonucu = db.Column(db.Integer, nullable=False)
+    su_sonucu = db.Column(db.Integer, nullable=False)
+    medikal_sonucu = db.Column(db.Integer, nullable=False)
+    ekip_sonucu = db.Column(db.Integer, nullable=False)
+    
     kayit_tarihi = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
 
-    afet_olayi = db.relationship('AfetOlayi', back_populates='tahmin_kayitlari')
-
     def __repr__(self):
-        return f'<TahminKaydi {self.id} - Olay:{self.afet_olayi_id}>'
+        return f'<TahminKaydi {self.id} - {self.kayit_tarihi}>'
--- repository.py
+++ repository.py
@@ -1,59 +1,20 @@
-from models import db, Bolge, AfetOlayi, TahminKaydi
-
-
-class BolgeRepository:
-    @staticmethod
-    def kaydet(ad, il, nufus, ilce=None, lat=None, lon=None):
-        bolge = Bolge(ad=ad, il=il, ilce=ilce, nufus=nufus,
-                      koordinat_lat=lat, koordinat_lon=lon)
-        db.session.add(bolge)
-        db.session.commit()
-        return bolge.id
-
-    @staticmethod
-    def hepsini_getir():
-        return Bolge.query.all()
-
-    @staticmethod
-    def id_ile_getir(bolge_id):
-        return Bolge.query.get(bolge_id)
-
-
-class AfetOlayiRepository:
-    @staticmethod
-    def kaydet(bolge_id, veri):
-        olay = AfetOlayi(
-            bolge_id=bolge_id,
-            deprem_buyuklugu=veri['deprem_buyuklugu'],
-            bina_yikim_orani=veri['bina_yikim_orani'],
-            hava_sicakligi=veri['hava_sicakligi'],
-            ulasim_durumu=veri['ulasim_durumu'],
-            yasli_nufus_orani=veri['yasli_nufus_orani'],
-        )
-        db.session.add(olay)
-        db.session.commit()
-        return olay.id
-
-    @staticmethod
-    def id_ile_getir(olay_id):
-        return AfetOlayi.query.get(olay_id)
-
+from models import db, TahminKaydi
 
 class TahminRepository:
     @staticmethod
-    def kaydet(afet_olayi_id, tahmin_sonuclari):
-        kayit = TahminKaydi(
-            afet_olayi_id=afet_olayi_id,
-            acil_barinma=int(tahmin_sonuclari[0]),
-            gida=int(tahmin_sonuclari[1]),
-            su=int(tahmin_sonuclari[2]),
-            medikal=int(tahmin_sonuclari[3]),
-            ekip=int(tahmin_sonuclari[4]),
+    def kaydet(veri_sozlugu, tahmin_sonuclari):
+        yeni_kayit = TahminKaydi(
+            nufus=veri_sozlugu['nufus'],
+            bina_yikim_orani=veri_sozlugu['bina_yikim_orani'],
+            hava_sicakligi=veri_sozlugu['hava_sicakligi'],
+            ulasim_durumu=veri_sozlugu['ulasim_durumu'],
+            yasli_nufus_orani=veri_sozlugu['yasli_nufus_orani'],
+            barinma_sonucu=int(tahmin_sonuclari[0]),
+            gida_sonucu=int(tahmin_sonuclari[1]),
+            su_sonucu=int(tahmin_sonuclari[2]),
+            medikal_sonucu=int(tahmin_sonuclari[3]),
+            ekip_sonucu=int(tahmin_sonuclari[4])
         )
-        db.session.add(kayit)
+        db.session.add(yeni_kayit)
         db.session.commit()
-        return kayit.id
-
-    @staticmethod
-    def olay_id_ile_getir(afet_olayi_id):
-        return TahminKaydi.query.filter_by(afet_olayi_id=afet_olayi_id).all()
+        return yeni_kayit.id
