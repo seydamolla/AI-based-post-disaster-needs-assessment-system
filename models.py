68ed9b481ece01ce52942e5f09fb67114d2eb4a9
Seyda <seydamollaoglari@gmail.com>
2026-05-28 22:18:10 +0300

    KAN-19: Iliskisel veri modelleri - Bolge ve AfetOlayi tablolari eklendi

--- models.py
+++ models.py
@@ -3,23 +3,58 @@ from datetime import datetime, timezone
 
 db = SQLAlchemy()
 
+
+class Bolge(db.Model):
+    __tablename__ = 'bolgeler'
+
+    id = db.Column(db.Integer, primary_key=True)
+    ad = db.Column(db.String(100), nullable=False)
+    il = db.Column(db.String(50), nullable=False)
+    ilce = db.Column(db.String(50), nullable=True)
+    nufus = db.Column(db.Integer, nullable=False)
+    koordinat_lat = db.Column(db.Float, nullable=True)
+    koordinat_lon = db.Column(db.Float, nullable=True)
+
+    afet_olaylari = db.relationship('AfetOlayi', back_populates='bolge', lazy=True)
+
+    def __repr__(self):
+        return f'<Bolge {self.ad} - {self.il}>'
+
+
+class AfetOlayi(db.Model):
+    __tablename__ = 'afet_olaylari'
+
+    id = db.Column(db.Integer, primary_key=True)
+    bolge_id = db.Column(db.Integer, db.ForeignKey('bolgeler.id'), nullable=False)
+    deprem_buyuklugu = db.Column(db.Float, nullable=False)
+    bina_yikim_orani = db.Column(db.Float, nullable=False)
+    hava_sicakligi = db.Column(db.Float, nullable=False)
+    ulasim_durumu = db.Column(db.Integer, nullable=False)    # 0=kapali, 1=kismi, 2=acik
+    yasli_nufus_orani = db.Column(db.Float, nullable=False)
+    olay_tarihi = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+
+    bolge = db.relationship('Bolge', back_populates='afet_olaylari')
+    tahmin_kayitlari = db.relationship('TahminKaydi', back_populates='afet_olayi', lazy=True)
+
+    def __repr__(self):
+        return f'<AfetOlayi {self.id} - Buyukluk:{self.deprem_buyuklugu}>'
+
+
 class TahminKaydi(db.Model):
     __tablename__ = 'tahmin_kayitlari'
 
     id = db.Column(db.Integer, primary_key=True)
-    nufus = db.Column(db.Integer, nullable=False)
-    bina_yikim_orani = db.Column(db.Float, nullable=False)
-    hava_sicakligi = db.Column(db.Float, nullable=False)
-    ulasim_durumu = db.Column(db.Integer, nullable=False)
-    yasli_nufus_orani = db.Column(db.Float, nullable=False)
-    
-    barinma_sonucu = db.Column(db.Integer, nullable=False)
-    gida_sonucu = db.Column(db.Integer, nullable=False)
-    su_sonucu = db.Column(db.Integer, nullable=False)
-    medikal_sonucu = db.Column(db.Integer, nullable=False)
-    ekip_sonucu = db.Column(db.Integer, nullable=False)
-    
+    afet_olayi_id = db.Column(db.Integer, db.ForeignKey('afet_olaylari.id'), nullable=False)
+
+    acil_barinma = db.Column(db.Integer, nullable=False)
+    gida = db.Column(db.Integer, nullable=False)
+    su = db.Column(db.Integer, nullable=False)
+    medikal = db.Column(db.Integer, nullable=False)
+    ekip = db.Column(db.Integer, nullable=False)
+
     kayit_tarihi = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
 
+    afet_olayi = db.relationship('AfetOlayi', back_populates='tahmin_kayitlari')
+
     def __repr__(self):
-        return f'<TahminKaydi {self.id} - {self.kayit_tarihi}>'
+        return f'<TahminKaydi {self.id} - Olay:{self.afet_olayi_id}>'
